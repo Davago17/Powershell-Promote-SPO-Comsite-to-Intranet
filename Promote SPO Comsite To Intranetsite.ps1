9af491fed8ad632b012bc7cc8c3bd8b9ae3a3c22
Davago17 <dave.vangool@gmail.com>
2022-09-08 12:23:02 +0200

    Temp update

--- Promote SPO Comsite To Intranetsite.ps1
+++ Promote SPO Comsite To Intranetsite.ps1
@@ -1,5 +1,4 @@
-<#
-PowerShell Script writen by Dave Van Gool - Consultant The Flow (Cronos Group)
+<# PowerShell Script writen by Dave Van Gool - Consultant The Flow (Cronos Group)
 This script connects to sharepoint online and promote a Communication page to be the intranet main page.
 For this script the module pnp.powerShell is used.
 #>
@@ -8,7 +7,11 @@ $IntranetPage = Read-Host -Prompt "Please provide the url to the sharepoint site
 
 Connect-PnPOnline -Url $IntranetPage -LaunchBrowser -Interactive
 $UrlSplit = $IntranetPage -split "sites"
-$SiteName = $UrlSplit[1].replace("/","")
+$SiteName = $UrlSplit[1].replace("/", "")
 $Tenant = $UrlSplit[0]
 Write-Output "connected to $SiteName(Tenant: $Tenant)"
 
+$Verify = Read-Host -Prompt "$IntranetPage is this url correct (Y/N)" 
+if ($Verify -eq "Y") {
+    $HomesiteCheck = Get-PnPHomeSite
+}
