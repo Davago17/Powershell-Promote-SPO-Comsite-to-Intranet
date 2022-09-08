acb7782326e5f51f4cc5c14dd368798f8539143c
Davago17 <dave.vangool@gmail.com>
2022-09-08 13:32:53 +0200

    Final version of the script not tested

--- Promote SPO Comsite To Intranetsite.ps1
+++ Promote SPO Comsite To Intranetsite.ps1
@@ -4,6 +4,7 @@ For this script the module pnp.powerShell is used.
 #>
 
 $IntranetPage = Read-Host -Prompt "Please provide the url to the sharepoint site that needs to be promoted" 
+Connect-PnPOnline -Url $Tenant -Interactive -LaunchBrowser
 
 Connect-PnPOnline -Url $IntranetPage -LaunchBrowser -Interactive
 $UrlSplit = $IntranetPage -split "sites"
@@ -14,4 +15,25 @@ Write-Output "connected to $SiteName(Tenant: $Tenant)"
 $Verify = Read-Host -Prompt "$IntranetPage is this url correct (Y/N)" 
 if ($Verify -eq "Y") {
     $HomesiteCheck = Get-PnPHomeSite
+    if ($HomesiteCheck -eq $IntranetPage) {
+        <#The Url provided is already defined as homesite#>
+        Write-Output "The $IntranetPage is already assigned as intranet home site"
+    }
+    elseif ($Null -eq $HomesiteCheck) {
+        <#If no Home Site is defined#>
+        Write-Output "There is no Home Site defined , $IntranetPage will be the home site"
+        Set-PnPHomeSite -HomeSiteUrl $IntranetPage 
+        Write-Output "The $IntranetPage is assigned as the home site"
+    }
+    elseif ($HomesiteCheck -ne $IntranetPage) {
+        <# If there is a site already defined as homesite request user if the Homesite will be changed to the Url provided#>
+        Write-Output "The Home Site is $HomesiteCheck"
+        $ReplaceHomeSite = Read-Host -Prompt "Do you want to change the $IntranetPage with $HomesiteCheck as default Home Site (Y/N)"
+        Switch ($ReplaceHomeSite) {
+            Y { Write-Output "Setting the $IntranetPage as default Home Site" | Set-PnPHomeSite -HomeSiteUrl $IntranetPage | Write-Output "The $IntranetPage is assigned as the home site" }
+            N { Write-Output "The home site remains $HomesiteCheck" | break }
+            Default { Write-Output "Didn't recognize your answer as a valid option" }
+        }
+    }
 }
+
